ff0f3b5d3eac2091eb11fa89fbb541f4d494176a
jhonatanT04 <venotacu@gmail.com>
2024-10-29 19:56:45 -0500

    Firebase no import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {initializeApp} from 'firebase/app';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {} from '@angular/'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOZ-vpnqt1ls05BBkIIE4GMTSK4BxO2BQ",
+  authDomain: "proyectointerciclo-5b9a1.firebaseapp.com",
+  projectId: "proyectointerciclo-5b9a1",
+  storageBucket: "proyectointerciclo-5b9a1.appspot.com",
+  messagingSenderId: "220944298224",
+  appId: "1:220944298224:web:4ae3eeff99f23079d5d235",
+  measurementId: "G-J9NCNFVHYW"
+};
+
+initializeApp(firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
+    importProvidersFrom(HttpClientModule,AngularFireModule)
+  ]
 };
