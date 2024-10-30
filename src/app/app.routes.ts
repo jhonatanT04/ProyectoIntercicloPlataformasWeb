cb5f8bf34634a5009d92c1335bcc99e44cc89e32
JustinLucero07 <justinluceroxd04@gmail.com>
2024-10-30 08:58:37 -0500

    Creacion de componentes: Login, Pantalla Carga, adicional se realiza el router link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,25 @@
 import { Routes } from '@angular/router';
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { AdministadoresComponent } from './components/administadores/administadores.component';
+import { GestionEspaciosComponent } from './pages/gestion-espacios/gestion-espacios.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PantallaCargaComponent } from './pages/pantalla-carga/pantalla-carga.component';
 
 export const routes: Routes = [
     {
         path: 'pages/usuarios',
         component: UsuariosComponent
+    },
+    {
+        path: 'pages/gestion',
+        component: GestionEspaciosComponent
+    },
+    {
+        path: 'pages/login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        component: PantallaCargaComponent
     }
 ];
--- src/app/pages/gestion-espacios/gestion-espacios.component.ts
+++ src/app/pages/gestion-espacios/gestion-espacios.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-gestion-espacios',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './gestion-espacios.component.html',
   styleUrl: './gestion-espacios.component.scss'
 })
