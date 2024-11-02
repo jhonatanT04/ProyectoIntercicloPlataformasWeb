b1d72e37b12dc25a4e3fd2d7eef5cb5f39c26394
JustinLucero07 <justinluceroxd04@gmail.com>
2024-11-02 16:38:55 -0500

    Se realiza los metodos para agregar, eliminar espacios tanto en el servicio como en el componente

--- src/app/pages/gestion-espacios/gestion-espacios.component.ts
+++ src/app/pages/gestion-espacios/gestion-espacios.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AdministradoresServiceService } from '../../services/administradores-service.service';
 
 @Component({
   selector: 'app-gestion-espacios',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule,CommonModule],
   templateUrl: './gestion-espacios.component.html',
   styleUrl: './gestion-espacios.component.scss'
 })
-export class GestionEspaciosComponent {
+export class GestionEspaciosComponent implements OnInit{
 
+  tipo = ''
+  nombre= ''
+  espacios:any =[]
+  constructor(private espacioS:AdministradoresServiceService){}
+  ngOnInit(): void {
+    this.cargarEs()
+  }
+
+  cargarEs(){
+    this.espacios = this.espacioS.cargarEspacios()
+  }
+
+  agregarEspacio(){
+    this.espacioS.agregarEspacio(this.nombre,this.tipo)
+    this.cargarEs() 
+    this.tipo=''
+    this.nombre=''
+  }
+
+  eliminarEspacio(espacio:any){
+    this.espacioS.eliminarEspacio(espacio)
+    this.cargarEs()
+  }
 }
--- src/app/services/administradores-service.service.ts
+++ src/app/services/administradores-service.service.ts
@@ -47,4 +47,63 @@ export class AdministradoresServiceService {
     }
   }
   
+  listaEspacios: any =[]
+
+  agregarEspacio(nombre:string,tipo:string){
+    this.listaEspacios.push({nombre,tipo})
+    localStorage.setItem('listEspacios',JSON.stringify(this.listaEspacios)) 
+  }
+
+  eliminarEspacio(espacio:any){
+    const index = this.listaEspacios.indexOf(espacio)
+    if(index>-1){
+      this.listaEspacios.splice(index,1)
+    } 
+    localStorage.setItem('listEspacios',JSON.stringify(this.listaEspacios))
+  }
+
+  cargarEspacios(){
+    return this.listaEspacios
+  }
+
+  listaContratos: any =[]
+
+  agregarContrato(nombreC:string,nombreE:string,duracion:number,tarifa:number){
+    this.listaContratos.push({nombreC,nombreE,duracion,tarifa})
+    localStorage.setItem('listContratos',JSON.stringify(this.listaContratos))
+  }
+
+  eliminarContrato(contrato:any){
+    const index = this.listaContratos.indexOf(contrato)
+    if(index>-1){
+      this.listaContratos.splice(index,1)
+    } 
+    localStorage.setItem('listContratos',JSON.stringify(this.listaEspacios))
+  }
+
+  cargarContratos(){
+    return this.listaContratos
+  }
+
+  listaTarifa: any=[]
+  agregarTarifa(tipo:string,costo:number){
+    this.listaTarifa.push({tipo,costo})
+    localStorage.setItem('listTarifas',JSON.stringify(this.listaTarifa))
+  }  
+
+  eliminarTarifa(tarifa:any){
+    const index = this.listaTarifa.indexOf(tarifa)
+    if(index>-1){
+      this.listaTarifa.splice(index,1)
+    } 
+    localStorage.setItem('listTarifas',JSON.stringify(this.listaEspacios))
+  }
+
+  cargarTarifa(){
+    return this.listaTarifa
+  }
+
+  listaHorarios:any =[]
+
+  agrgarHorario(){}
 }
