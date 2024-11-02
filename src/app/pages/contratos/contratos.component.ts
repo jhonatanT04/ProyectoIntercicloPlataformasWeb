da5a45687806a1a5e5f0d7e000e652692953c9bc
JustinLucero07 <justinluceroxd04@gmail.com>
2024-11-02 17:48:49 -0500

    Por el momento se escribe el nombre del cliente, la idea es que se cargue de la lista de user que se tiene

--- src/app/pages/contratos/contratos.component.ts
+++ src/app/pages/contratos/contratos.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AdministradoresServiceService } from '../../services/administradores-service.service';
 
 @Component({
   selector: 'app-contratos',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './contratos.component.html',
   styleUrl: './contratos.component.scss'
 })
-export class ContratosComponent {
+export class ContratosComponent implements OnInit{
 
+  espacios: any =[]
+  contratos: any = []
+  clientes:any =  []
+
+  cliente=''
+  espacio=''
+  duracion=0
+
+  constructor(private contratoS:AdministradoresServiceService){}
+  ngOnInit(): void {
+    this.cargarContratos()
+    this.cargarEspacios()
+  }
+
+  cargarClientes(){
+    
+  }
+  cargarEspacios(){
+    this.espacios = this.contratoS.cargarEspacios()
+  }
+
+  cargarContratos(){
+    this.contratos =this.contratoS.cargarContratos()
+  }
+
+  agregarContrato(){
+    this.contratoS.agregarContrato(this.cliente,this.espacio,this.duracion,1)
+    localStorage.setItem('listContratos',JSON.stringify(this.contratos))
+    this.cargarContratos() 
+  }
+
+  eliminarContrato(contrato:any){
+
+  }
 }
