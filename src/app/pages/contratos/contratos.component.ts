2efbd99bc090abb0130b2fafa066deca07447f9a
JustinLucero07 <justinluceroxd04@gmail.com>
2024-11-02 18:17:56 -0500

    Se realiza de forma correcta el eliminar y agregar contrato

--- src/app/pages/contratos/contratos.component.ts
+++ src/app/pages/contratos/contratos.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AdministradoresServiceService } from '../../services/administradores-service.service';
+import { UsuariosServiceService } from '../../services/usuarios-service.service';
 
 @Component({
   selector: 'app-contratos',
@@ -20,14 +21,15 @@ export class ContratosComponent implements OnInit{
   espacio=''
   duracion=0
 
-  constructor(private contratoS:AdministradoresServiceService){}
+  constructor(private contratoS:AdministradoresServiceService,private clienteS:UsuariosServiceService){}
   ngOnInit(): void {
     this.cargarContratos()
     this.cargarEspacios()
+    this.cargarClientes() 
   }
 
   cargarClientes(){
-    
+    this.clientes = this.clienteS.cargarUsuario() 
   }
   cargarEspacios(){
     this.espacios = this.contratoS.cargarEspacios()
@@ -44,6 +46,8 @@ export class ContratosComponent implements OnInit{
   }
 
   eliminarContrato(contrato:any){
-
+    this.contratoS.eliminarContrato(contrato)
+    localStorage.setItem('listContratos',JSON.stringify(this.contratos))
+    this.cargarContratos() 
   }
 }
