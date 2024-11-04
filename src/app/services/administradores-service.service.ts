246bf7073028c05d06a1459fe3b44dac9cf134fe
JustinLucero07 <justinluceroxd04@gmail.com>
2024-11-04 15:29:08 -0500

    se modifican todos los metodos del servicio de administradores agregando una condicion con el isPlatform y evitar que nos de el error del local storage

--- src/app/services/administradores-service.service.ts
+++ src/app/services/administradores-service.service.ts
@@ -1,124 +1,158 @@
-import { Injectable } from '@angular/core';
-import { Persona } from '../models/persona';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core'; import { Persona } from '../models/persona';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdministradoresServiceService {
 
-  constructor() {
-    this.cargarAdmi()
-   }
+  listaAdministradores: Persona[] = [];
+  listaEspacios: any = [];
+  listaContratos: any = [];
+  listaTarifa: any = [];
+  listaHorarios: any = [];
 
-  listaAdministradores: Persona[] =[]
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.cargarAdmi();
+    }
+  }
+
   cargarAdmi(): Persona[] {
-    const storedList = localStorage.getItem('listAdm');
-    if (!storedList || JSON.parse(storedList).length === 0) {
-      const administradorPorDefecto = new Persona(
-        'admin@example.com',     
-        'admin123',               
-        'Admin',                  
-        'Principal',              
-        '123456789',              
-        '123 Calle Falsa',        
-        'ADM001',                 
-        'Ecuador',                
-        'Quito',                  
-        true                      
-      );
-      this.listaAdministradores = [administradorPorDefecto];
-      localStorage.setItem('listAdm', JSON.stringify(this.listaAdministradores));
-    } else {
-      this.listaAdministradores = JSON.parse(storedList);
+    if (isPlatformBrowser(this.platformId)) {
+      const storedList = localStorage.getItem('listAdm');
+      if (!storedList || JSON.parse(storedList).length === 0) {
+        const administradorPorDefecto = new Persona(
+          'admin@example.com',
+          'admin123',
+          'Admin',
+          'Principal',
+          '123456789',
+          '123 Calle Falsa',
+          'ADM001',
+          'Ecuador',
+          'Quito',
+          true
+        );
+        this.listaAdministradores = [administradorPorDefecto];
+        localStorage.setItem('listAdm', JSON.stringify(this.listaAdministradores));
+      } else {
+        this.listaAdministradores = JSON.parse(storedList);
+      }
     }
     return this.listaAdministradores;
   }
-  nuevoAdmi(adminstrador:Persona) {
-    let cur = {  }
-    this.listaAdministradores.push(adminstrador);
-    localStorage.setItem('listAdm', JSON.stringify(this.listaAdministradores))
-  }
-  eliminarAdmi(adminstrador:Persona) {
-    const index = this.listaAdministradores.indexOf(adminstrador);
-    if (index !== -1) {
-      this.listaAdministradores.splice(index, 1);
-      console.log(this.listaAdministradores)
+
+  nuevoAdmi(adminstrador: Persona) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.listaAdministradores.push(adminstrador);
       localStorage.setItem('listAdm', JSON.stringify(this.listaAdministradores));
     }
   }
-  
-  listaEspacios: any =[]
 
-  agregarEspacio(nombre:string,tipo:string){
-    this.listaEspacios.push({nombre,tipo})
-    localStorage.setItem('listEspacios',JSON.stringify(this.listaEspacios)) 
+  eliminarAdmi(adminstrador: Persona) {
+    if (isPlatformBrowser(this.platformId)) {
+      const index = this.listaAdministradores.indexOf(adminstrador);
+      if (index !== -1) {
+        this.listaAdministradores.splice(index, 1);
+        localStorage.setItem('listAdm', JSON.stringify(this.listaAdministradores));
+      }
+    }
   }
 
-  eliminarEspacio(espacio:any){
-    const index = this.listaEspacios.indexOf(espacio)
-    if(index>-1){
-      this.listaEspacios.splice(index,1)
-    } 
-    localStorage.setItem('listEspacios',JSON.stringify(this.listaEspacios))
+  agregarEspacio(nombre: string, tipo: string) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.listaEspacios.push({ nombre, tipo });
+      localStorage.setItem('listEspacios', JSON.stringify(this.listaEspacios));
+    }
   }
 
-  cargarEspacios(){
-    return this.listaEspacios
+  eliminarEspacio(espacio: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      const index = this.listaEspacios.indexOf(espacio);
+      if (index > -1) {
+        this.listaEspacios.splice(index, 1);
+        localStorage.setItem('listEspacios', JSON.stringify(this.listaEspacios));
+      }
+    }
   }
 
-  listaContratos: any =[]
-
-  agregarContrato(nombreC:string,nombreE:string,duracion:number,tarifa:number){
-    this.listaContratos.push({nombreC,nombreE,duracion,tarifa})
-    localStorage.setItem('listContratos',JSON.stringify(this.listaContratos))
+  cargarEspacios() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.listaEspacios = JSON.parse(localStorage.getItem('listEspacios') || '[]');
+    }
+    return this.listaEspacios;
   }
 
-  eliminarContrato(contrato:any){
-    const index = this.listaContratos.indexOf(contrato)
-    if(index>-1){
-      this.listaContratos.splice(index,1)
-    } 
-    localStorage.setItem('listContratos',JSON.stringify(this.listaEspacios))
+  agregarContrato(nombreC: string, nombreE: string, duracion: number, tarifa: number) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.listaContratos.push({ nombreC, nombreE, duracion, tarifa });
+      localStorage.setItem('listContratos', JSON.stringify(this.listaContratos));
+    }
   }
 
-  cargarContratos(){
-    return this.listaContratos
+  eliminarContrato(contrato: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      const index = this.listaContratos.indexOf(contrato);
+      if (index > -1) {
+        this.listaContratos.splice(index, 1);
+        localStorage.setItem('listContratos', JSON.stringify(this.listaContratos));
+      }
+    }
   }
 
-  listaTarifa: any=[]
-  agregarTarifa(tipo:string,costo:number){
-    this.listaTarifa.push({tipo,costo})
-    localStorage.setItem('listTarifas',JSON.stringify(this.listaTarifa))
-  }  
-
-  eliminarTarifa(tarifa:any){
-    const index = this.listaTarifa.indexOf(tarifa)
-    if(index>-1){
-      this.listaTarifa.splice(index,1)
-    } 
-    localStorage.setItem('listTarifas',JSON.stringify(this.listaEspacios))
+  cargarContratos() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.listaContratos = JSON.parse(localStorage.getItem('listContratos') || '[]');
+    }
+    return this.listaContratos;
   }
 
-  cargarTarifa(){
-    return this.listaTarifa
+  agregarTarifa(tipo: string, costo: number) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.listaTarifa.push({ tipo, costo });
+      localStorage.setItem('listTarifas', JSON.stringify(this.listaTarifa));
+    }
   }
 
-  listaHorarios:any =[]
-
-  agrgarHorario(dia:string,horaA:string,horaC:string){
-    this.listaHorarios.push({dia,horaA,horaC})
-    localStorage.setItem('listHorarios',JSON.stringify(this.listaHorarios))
+  eliminarTarifa(tarifa: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      const index = this.listaTarifa.indexOf(tarifa);
+      if (index > -1) {
+        this.listaTarifa.splice(index, 1);
+        localStorage.setItem('listTarifas', JSON.stringify(this.listaTarifa));
+      }
+    }
   }
 
-  eliminarHorario(horario:any){
-    const index = this.listaHorarios.indexOf(horario)
-    if(index>-1){
-      this.listaHorarios.splice(index,1)
-    } 
-    localStorage.setItem('listHorarios',JSON.stringify(this.listaHorarios))
+  cargarTarifa() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.listaTarifa = JSON.parse(localStorage.getItem('listTarifas') || '[]');
+    }
+    return this.listaTarifa;
   }
 
-  cargarHorario(){
-    return this.listaHorarios
+  agrgarHorario(dia: string, horaA: string, horaC: string) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.listaHorarios.push({ dia, horaA, horaC });
+      localStorage.setItem('listHorarios', JSON.stringify(this.listaHorarios));
+    }
+  }
+
+  eliminarHorario(horario: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      const index = this.listaHorarios.indexOf(horario);
+      if (index > -1) {
+        this.listaHorarios.splice(index, 1);
+        localStorage.setItem('listHorarios', JSON.stringify(this.listaHorarios));
+      }
+    }
+  }
+
+  cargarHorario() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.listaHorarios = JSON.parse(localStorage.getItem('listHorarios') || '[]');
+    }
+    return this.listaHorarios;
   }
 }
