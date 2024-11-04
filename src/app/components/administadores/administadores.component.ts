d6d49ef3aeed4bf1ff33cbbd1cf4290d02ac5827
JustinLucero07 <justinluceroxd04@gmail.com>
2024-11-04 15:17:00 -0500

    Se modifica la parte de abrir cada componente, ya no se utiliza router module

--- src/app/components/administadores/administadores.component.ts
+++ src/app/components/administadores/administadores.component.ts
@@ -1,13 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthentificServiceService } from '../../services/authentific-service.service';
-
-
+import { TarifasComponent } from '../../pages/tarifas/tarifas.component';
+import { HorariosComponent } from '../../pages/horarios/horarios.component';
+import { ContratosComponent } from '../../pages/contratos/contratos.component';
+import { ListaUsuariosComponent } from '../../pages/lista-usuarios/lista-usuarios.component';
+import { GestionEspaciosComponent } from '../../pages/gestion-espacios/gestion-espacios.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-administadores',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,TarifasComponent,HorariosComponent,ContratosComponent,ListaUsuariosComponent,GestionEspaciosComponent,CommonModule],
   templateUrl: './administadores.component.html',
   styleUrl: './administadores.component.scss'
 })
@@ -23,5 +27,21 @@ export class AdministadoresComponent {
   }
   agrandar(){
     this.isSize = !this.isSize;
+  }
+  
+  espacios: boolean = false
+  contratos: boolean = false
+  tarifas: boolean = false
+  horarios: boolean = false
+  clientes: boolean = false
+
+  seccionabierta: string = ''; 
+
+  abrirSeccion(seccion: string) {
+    this.seccionabierta = this.seccionabierta === seccion ? '' : seccion; 
+  }
+
+  seccionAbierta(seccion: string): boolean {
+    return this.seccionabierta === seccion;
   }
 }
