5a7e757432af0083cf5876692d476d99292f1fdb
jhonatanT04 <venotacu@gmail.com>
2025-02-18 14:59:14 -0500

    Se corrije error de servicePersona

--- src/app/guards/guards-auth.guard.ts
+++ src/app/guards/guards-auth.guard.ts
@@ -3,14 +3,12 @@ import { CanActivateFn, CanMatchFn, Router } from '@angular/router';
 import { AuthentificServiceService } from '../services/authentific-service.service';
 
 export const guardsAuthGuard: CanMatchFn = (route, state) => {
-  // const router = inject(Router)
-  // const authServi = inject(AuthentificServiceService)
-  // if(authServi.isAutheticate()){
-  //   return true
-  // }else{
-  //   router.navigate(['pages/login'])
-  //   return false;
-  // }  
-  return true;
-  // return authServi.isAutheticate();
+  const router = inject(Router)
+  const authServi = inject(AuthentificServiceService)
+  if(authServi.isAutheticate()){
+    return true
+  }else{
+    router.navigate(['pages/login'])
+    return false;
+  }  
 };
--- src/app/services/usuarios-service.service.ts
+++ src/app/services/usuarios-service.service.ts
@@ -16,22 +16,32 @@ export class UsuariosServiceService {
   private apiUrl = 'http://localhost:8080/practica/rs/personas'; 
   
   createPersona(persona: Persona): Observable<Persona> {
-    return this.http.post<Persona>(this.apiUrl, persona);
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post<Persona>(this.apiUrl, persona, { headers });
   }
   updatePersona(persona: Persona): Observable<void> {
-    return this.http.put<void>(this.apiUrl, persona);
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.put<void>(this.apiUrl, persona, { headers });
   }
   
   getPersonaById(id: number): Observable<Persona> {
-    return this.http.get<Persona>(`${this.apiUrl}/${id}`);
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<Persona>(`${this.apiUrl}/${id}`, { headers });
   } 
 
   getPersonas(): Observable<Persona[]> {
-    return this.http.get<Persona[]>(`${this.apiUrl}`);
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<Persona[]>(`${this.apiUrl}`, { headers });
   }
   
   deletePersona(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers });
   }
   getPerfil(): Observable<Persona> {
     const token = localStorage.getItem('token');
